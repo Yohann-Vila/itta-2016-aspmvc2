68500be78b98ac10ab168b2071481a75080d27b6
Yohann-Vila <yohann.vila@signatys.com>
2016-04-04 11:17:13 +0200

    Stub for CatThreadDAO

---
+++ CatExercise/CatExercise/Dao/CatThreadDAO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CodeFirstCat;
+
+namespace CatExercise.Dao
+{
+    public class CatThreadDAO : ICatThreadDAO
+    {
+        public CatThread FindByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<CatThread> FindByLogin(string login, bool actif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<CatThread> FindByTitle(string partialTitle, bool actif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<CatThread> GetAll(bool actif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(CatThread catThread)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CatThread catThread)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
