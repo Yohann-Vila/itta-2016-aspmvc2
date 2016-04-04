b3381ae60f4e2149c2619df0160f80068bd6a15c
HuguesM <hugues.martinelli@gmail.com>
2016-04-04 11:47:04 +0200

    Add Factory

---
+++ CatExercise/CatExercise/Dao/DAOFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatExercise.Dao {
+    public static class DAOFactory {
+    private static String _SOURCE = "Entity";
+
+    public static IUserDAO getInstanceOfUser() {
+        switch (_SOURCE) {
+            case "Entity":
+                throw new NotImplementedException();
+            default:
+                throw new NotImplementedException();
+        }
+    }
+    public static ICatThreadDAO getInstanceOfCatThread() {
+        switch (_SOURCE) {
+            case "Entity":
+                return new CatThreadDAO();
+            default:
+                return new CatThreadDAO();
+        }
+    }
+    public static ICommentDAO getInstanceOfComment() {
+        switch (_SOURCE) {
+            case "Entity":
+                return new CommentDAO();
+            default:
+                return new CommentDAO();
+        }
+    }
+    }
+}
