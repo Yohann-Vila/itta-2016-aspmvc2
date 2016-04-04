4462c90165fa8c820b80ba31856167f2fb82d52d
ittastudy <ittastudy>
2016-04-04 03:19:33 -0700

    Methode abstr. IUSERDAO

--- CatExercise/CatExercise/Dao/IUserDAO.cs
+++ CatExercise/CatExercise/Dao/IUserDAO.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using CodeFirstCat;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace CatExercise.Dao {
     public interface IUserDAO {
+        ICollection<User> GetAll();
+        User Find(String login);
+        bool Insert(User user;
+        bool Update(User user);
     }
 }
