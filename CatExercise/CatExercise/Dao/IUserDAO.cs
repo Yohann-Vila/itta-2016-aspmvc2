0ecbaefe6b8bd112b3feb3632b311301858ea97a
ittastudy <ittastudy>
2016-04-04 03:24:14 -0700

    correction IUserDAO

--- CatExercise/CatExercise/Dao/IUserDAO.cs
+++ CatExercise/CatExercise/Dao/IUserDAO.cs
@@ -8,7 +8,7 @@ namespace CatExercise.Dao {
     public interface IUserDAO {
         ICollection<User> GetAll();
         User Find(String login);
-        bool Insert(User user;
+        bool Insert(User user);
         bool Update(User user);
     }
 }
