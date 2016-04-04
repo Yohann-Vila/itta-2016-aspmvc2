ba6715d49b9c33c2974988c65b825baa484ef2df
HuguesM <hugues.martinelli@gmail.com>
2016-04-04 16:29:11 +0200

    Add razor layout etc..

---
+++ CatExercise/CatExercise/Controllers/CommentController.cs
@@ -0,0 +1,22 @@
+﻿using CatExercise.Dao;
+using CatExercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatExercise.Controllers
+{
+    public class CommentController : Controller
+    {
+        // GET: Comment
+        public ActionResult Index()
+        {
+            ICommentDAO dao = DAOFactory.getInstanceOfComment();
+            ICollection<CommentView> cms = dao.getPostsFromThread(1, false);
+            return View(cms);
+
+        }
+    }
+}
