3e7d5e8650e421a8185a799425da424fe1704b8a
ittastudy <ittastudy>
2016-04-04 03:04:47 -0700

    Création UserDAO

---
+++ CatExercise/CatExercise/Dao/UserDAO.cs
@@ -0,0 +1,40 @@
+﻿using CodeFirstCat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatExercise.Dao
+{
+    public class UserDAO : IUserDAO
+    {
+        //
+        // GET: /Default1/
+
+        public User Find(String login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<User> getAll(string login, bool actif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool insert(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(CatThread catThread)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
