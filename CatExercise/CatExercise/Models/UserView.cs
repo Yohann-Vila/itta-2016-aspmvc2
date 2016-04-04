3e044e8217aa8737943b0b663ca005c52413dc16
ittastudy <ittastudy>
2016-04-04 03:31:42 -0700

    Vue du User

--- CatExercise/CatExercise/Models/UserView.cs
+++ CatExercise/CatExercise/Models/UserView.cs
@@ -5,5 +5,12 @@ using System.Web;
 
 namespace CatExercise.Models {
     public class UserView {
+        public int UserID { get; set; }
+        public String Login { get; set; }
+        public String Password { get; set; }
+        public String Pseudo { get; set; }
+        public Boolean Banish { get; set; }
+        public int Seclevel { get; set; }
+        public DateTime? Creationdate { get; set; }
     }
 }
