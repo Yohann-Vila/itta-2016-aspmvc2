d34de2228bb503c7d689c556e78e124ed1f312ec
Yohann-Vila <yohann.vila@signatys.com>
2016-04-04 12:11:39 +0200

    Created CatThreadView

--- CatExercise/CatExercise/Models/CatThreadView.cs
+++ CatExercise/CatExercise/Models/CatThreadView.cs
@@ -5,5 +5,17 @@ using System.Web;
 
 namespace CatExercise.Models {
     public class CatThreadView {
+        public int CatThreadId { get; set; }
+        public UserView User { get; set; }
+        public String Titre { get; set; }
+        public String UriPhoto { get; set; }
+        public bool Deleted { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public ICollection<CommentView> Comments { get; set; }
+
+        public CatThreadView()
+        {
+
+        }
     }
 }
