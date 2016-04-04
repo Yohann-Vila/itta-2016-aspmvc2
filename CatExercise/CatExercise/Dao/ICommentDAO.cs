cb62565cc7300d802f8f582491e45407e3cbce8a
HuguesM <hugues.martinelli@gmail.com>
2016-04-04 11:19:00 +0200

    Add

--- CatExercise/CatExercise/Dao/ICommentDAO.cs
+++ CatExercise/CatExercise/Dao/ICommentDAO.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using CodeFirstCat;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CatExercise.Dao {
-    interface ICommentDAO {
+    public interface ICommentDAO {
+        public ICollection<Comment> getPostsFromThread(int IdThread, Boolean actif);
+        public Boolean insert(Comment c);
+        public Boolean update(Comment c);
+        public Comment findByID(int commentID);
     }
 }
