f0ed068d253e371ce7c7476b435ec839df5e936c
Yohann-Vila <yohann.vila@signatys.com>
2016-04-04 11:14:49 +0200

    added reference to model and addded iCatThread methods

--- CatExercise/CatExercise/Dao/ICatThreadDAO.cs
+++ CatExercise/CatExercise/Dao/ICatThreadDAO.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CodeFirstCat;
 
 namespace CatExercise.Dao {
     interface ICatThreadDAO {
+        public ICollection<CatThread> GetAll(bool actif);
+        public ICollection<CatThread> FindByLogin(String login, bool actif);
+        public ICollection<CatThread> FindByTitle(String partialTitle, bool actif);
+
+        public bool Update(CatThread catThread);
+        public bool Insert(CatThread catThread);
+        public CatThread FindByID(int id);
     }
 }
